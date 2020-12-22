1d717cc46f8974edb2f5382b08afb7e50f6cf771
Kristof Rado <rado.krisi@gmail.com>
2020-12-22 01:23:51 +0100

    Added safety check

--- eac2ac3.bat
+++ eac2ac3.bat
@@ -1,9 +1,13 @@
 @echo off
-del ".\log.txt"
+IF [%2] == [] (
+        echo "No language name specified" 
+        GOTO :eof
+    )
+IF EXIST ".\log.txt" del ".\log.txt"
 .\eac3to\eac3to.exe %1 2: .\temp\temp.eac3 -0dB || exit 1
 ren ".\temp\temp*.eac3" temp.eac3 || exit 1
 .\eac3to\eac3to .\temp\temp.eac3 stdout.w64 | .\ffmpeg\bin\ffmpeg -i - -c:a ac3 -b:a 640k -center_mixlev 0.707 .\temp\temp.ac3 || exit 1
-.\mkvtoolnix\mkvmerge.exe -o .\temp\muxed.mkv --language 0:eng %1 --default-track 0:true --language 0:eng .\temp\temp.ac3 || exit 1
+.\mkvtoolnix\mkvmerge.exe -o .\temp\muxed.mkv --language 0:%2 %1 --default-track 0:true --language 0:%2 .\temp\temp.ac3 || exit 1
 xcopy /y .\temp\muxed.mkv %1 || exit 1
 echo Waiting for 5 seconds...
 TIMEOUT 5
